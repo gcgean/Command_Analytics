2804fc77f2db27ea7fe8a1c35f131b14a35264b9
gcgean <81776909+gcgean@users.noreply.github.com>
2026-07-30 12:59:26 -0300

    dd

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-c5fd805d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.0hlsv26dkro"
+    "revision": "0.ai33ko4qkcs"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
